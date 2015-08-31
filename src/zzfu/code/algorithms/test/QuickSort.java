bed4cde93e73ae9ca0b30e397653fd019affa99a
zzfu <zzfu@Ctrip.com>
2015-08-31 18:31:54 +0800

    a

--- src/zzfu/code/algorithms/test/QuickSort.java
+++ src/zzfu/code/algorithms/test/QuickSort.java
@@ -1,10 +1,20 @@
 package zzfu.code.algorithms.test;
 
-public class QuickSort {
+public class QuickSort extends AbstractSort {
+	
+	
+	public QuickSort(int arraySize) {
+		super(arraySize); 
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void sort(int[] ints) {
+		
 	}
 
 }
